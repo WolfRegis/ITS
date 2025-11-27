0faa06c56e8dc674db93eda85387b57bc36ba292
WolfRegis <wolfregis2001@gmail.com>
2025-11-27 15:12:22 +0100

    ✨F09>add TrustContainer component & refacto Card component for mobile

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,24 +1,27 @@
 import React, { useRef } from 'react'
 import styled from 'styled-components';
 import theme from '../theme';
+import { IoIosArrowDown } from 'react-icons/io';
 
 export default function Card({titre, details}) {
 
-      const h2 = useRef(null)
-      const btn = useRef(null)
-
+      const card = useRef(null)
 
 
 
 
       const handleMouseEnter = ()=>{
-            h2.current.classList +=" activeH2"
-            btn.current.classList +=" activeBtn"
+            card.current.childNodes[0].classList +=" activeH2"
+            card.current.childNodes[2].classList +=" activeBtn" 
+            card.current.childNodes[1].classList.remove("hidden")
+            card.current.childNodes[2].classList.remove("hidden")
             
       }
       const handleMouseLeave = ()=>{
-            h2.current.classList.remove("activeH2")
-            btn.current.classList.remove("activeBtn")
+            card.current.childNodes[0].classList.remove("activeH2")
+            card.current.childNodes[1].classList.add("hidden")
+            card.current.childNodes[2].classList.add("hidden")
+            card.current.childNodes[2].classList.remove("activeBtn")
       }
 
 
@@ -27,10 +30,10 @@ export default function Card({titre, details}) {
 
 
   return (
-    <CardStyled className='relative flex flex-col lg:gap-6 gap-3 justify-center lg:w-90 w-50 shadow-lg h-full' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <h2 className='flex justify-center items-center lg:h-13 h-8 lg:text-lg text-xs font-bold text-white' ref={h2}>{titre}</h2>
-      <p className='mx-5 lg:text-[14px] text-xs text-center'>{details}</p>
-      <button className='grow-0 mb-6 self-center cursor-pointer font-bold lg:text-sm text-xs' ref={btn} >En savoir plus</button>
+    <CardStyled className='relative flex flex-col lg:gap-6 gap-3 justify-center lg:w-90 w-50 shadow-lg h-full' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} ref={card}>
+      <h2 className='flex lg:justify-center justify-between items-center lg:h-13 h-8 lg:text-lg text-xs font-bold text-white px-2.5'>{titre} <IoIosArrowDown className='relative lg:hidden' /></h2>
+      <p className='mx-5 lg:text-[14px] text-xs text-center lg:block hidden'>{details}</p>
+      <button className='grow-0 mb-6 self-center cursor-pointer font-bold lg:text-sm text-xs lg:block hidden' >En savoir plus</button>
     </CardStyled>
   )
 }
---
+++ src/components/ui/H1Black.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function H1Black({children}) {
+  return <h1 className='text-black font-black uppercase lg:text-4xl'>{children}</h1>
+}
